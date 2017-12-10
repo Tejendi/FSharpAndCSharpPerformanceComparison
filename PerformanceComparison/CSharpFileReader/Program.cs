cd77b001a51ff0dd647699aea1c83194670997ac
Daniel Bloch Christensen <daniel_b_christensen@hotmail.com>
2017-12-10 22:40:37 +0100

    Added projects

---
+++ PerformanceComparison/CSharpFileReader/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace CSharpFileReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Histogram>();
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class Histogram
+    {
+        [Benchmark]
+        public void PrintCharCount()
+        {
+            var path = @"..\..\RandomText.txt";
+            Dictionary<char, int> charCount = new Dictionary<char, int>()
+            {
+                { 'A', 0},
+                { 'B', 0},
+                { 'C', 0},
+                { 'D', 0},
+                { 'E', 0},
+                { 'F', 0},
+                { 'G', 0},
+                { 'H', 0},
+                { 'I', 0},
+                { 'J', 0},
+                { 'K', 0},
+                { 'L', 0},
+                { 'M', 0},
+                { 'N', 0},
+                { 'O', 0},
+                { 'P', 0},
+                { 'Q', 0},
+                { 'R', 0},
+                { 'S', 0},
+                { 'T', 0},
+                { 'U', 0},
+                { 'V', 0},
+                { 'W', 0},
+                { 'X', 0},
+                { 'Y', 0},
+                { 'Z', 0},
+            };
+
+            using (StreamReader fileContents = File.OpenText(path))
+            {
+                while (!fileContents.EndOfStream)
+                {
+                    char ch = Char.ToUpper((char)fileContents.Read());
+                    if (!Char.IsLetter(ch))
+                        continue;
+
+                    if (charCount.ContainsKey(ch))
+                    {
+                        charCount[ch]++;
+                    }
+                }
+            }
+
+            //Commented out code can be commented in again to show the result of the file read in the console
+
+            //foreach (var cCount in charCount)
+            //{
+            //    Console.WriteLine($"'{cCount.Key}': {cCount.Value} ");
+            //}
+        }
+    }
+
+}
---
+++ PerformanceComparison/FSharpFileReader/Program.fs
@@ -0,0 +1,32 @@
+﻿open System
+open System.IO
+open BenchmarkDotNet.Running
+open BenchmarkDotNet.Attributes
+
+[<MemoryDiagnoser>]
+type PerformanceTests_ReadFile() =
+    //let histogram path =
+    [<Benchmark>]
+    member this.ReadFiles() =
+        let path = @"..\..\RandomText.txt"
+        let charCount = [| for n in 'A'..'Z' -> 0 |]
+        let Acode = int 'A'
+        let file = File.OpenText path
+        while (not file.EndOfStream) do
+            let ch = char( file.Read() )
+            if (Char.IsLetter ch) then
+                let n = int (Char.ToUpper ch) - Acode
+                charCount.[n] <- charCount.[n] + 1
+            else ()
+        file.Close()
+
+        (*Below code can be commented in to show the results of the file read in the console*)
+        //let printOne n c = printf "%c: %4d\n" (char(n + Acode)) c
+        //Array.iteri printOne charCount
+
+[<EntryPoint>]
+let main argv = 
+    BenchmarkRunner.Run<PerformanceTests_ReadFile>() |> printfn "%A"
+    Console.ReadKey() |> ignore
+
+    0 // return an integer exit code
